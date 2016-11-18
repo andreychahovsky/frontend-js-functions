fffa35111dc8a8d97cc6279623912299513503bf
andreychahovsky <chahovsky.andrey@outlook.com>
2016-11-18 14:59:00 +0200

    function sum() is complete but I don't know exactly how to give variables to the functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-﻿
+﻿var arr, mm, array;
+var resmin, ressum;
+var i;
+var elements;
+
+// functions
+
+function sum() {
+	ressum = 0;
+	for (i = 0; i < array.length; i++) {
+		ressum += parseFloat(array[i]);
+	}
+}
+
+// start values
+
+ressum = 0;
+mm = [3,3,4];
+
+// code
+
+elements = prompt ('Please input array using separator ", ": ');
+arr = elements.split(', ');
+array = arr;
+sum(arr);
+console.log('array: ' + arr + '. sum(arr): ' + ressum);
+array = mm;
+sum(mm);
+console.log('array: ' + mm + '. sum(mm): ' + ressum);
+
+console.log('Fin.');
