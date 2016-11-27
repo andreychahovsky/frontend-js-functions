fa99a2f2d0c242737fcac48360275bc33414b348
andreychahovsky <chahovsky.andrey@outlook.com>
2016-11-27 22:36:36 +0200

    all functions without the error checking

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-﻿function sum() {
-	var array = [];
+function sum() {
 	var i = 0, ressum = 0;
 	for (i = 0; i < arguments.length; i++) {
-		array[i] = arguments[i];
-		ressum += parseFloat(array[i]);
+		if (typeof (arguments[i] = "number")) {
+			ressum += parseFloat(arguments[i]);
+		} else {
+
+		}
+
 	}
-	alert("sum: " + ressum);
+	return ('sum = ' + ressum);
+}
+
+function min() {
+	var i = 0, min;
+	for (i = 0; i < arguments.length; i++) {
+		min = arguments[0];
+	}
+	for (i = 1; i < arguments.length; i++) {
+		if (arguments[i] < min)	min = arguments[i];
+	}
+	return ('min = ' + min);
+}
+
+function max() {
+	var i = 0, max;
+	for (i = 0; i < arguments.length; i++) {
+		max = arguments[0];
+	}
+	for (i = 1; i < arguments.length; i++) {
+		if (arguments[i] > max)	max = arguments[i];
+	}
+	return ('max = ' + max);
 }
