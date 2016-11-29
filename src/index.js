a55530e595e4432dc3a3db58a2127ac4a8c64085
andreychahovsky <chahovsky.andrey@outlook.com>
2016-11-29 23:52:44 +0200

    don't pass one part of the tests min()

--- src/index.js
+++ src/index.js
@@ -1,41 +1,11 @@
 // funstion sum
 
-//function sum() {
-//	var i = 0, result = 0;
-//	var arr = [];
-//
-//	for (i = 0; i < arguments.length; i++) {
-//		arr[i] = parseFloat(arguments[i]);
-//	}	
-//	
-//	arr.reduce(function(previousValue, currentItem) {
-//		result = previousValue + currentItem;
-//		return result;
-//	}, 0);
-//	return result;
-//}
-
-//function sum() {
-//	var i = 0, result = 0;
-//	var arr = [];
-//
-//	for (i = 0; i < arguments.length; i++) {
-//		arr[i] = arguments[i];
-//	}	
-//	
-//	arr.reduce(function(previousValue, currentItem) {
-//		if (typeof(currentItem) == "number") {
-//			result = previousValue + currentItem;
-//			return result;
-//		}
-//	}, 0);
-//	return result;
-//}
-
 function sum() {
 	var i = 0, result = 0;
 	for (i = 0; i < arguments.length; i++) {
-		if (typeof(arguments[i]) !== "number") continue;
+		if (typeof(arguments[i]) !== "number") {
+			continue;
+		}
 		result += arguments[i];
 	}
 	return result;
@@ -44,60 +14,49 @@ function sum() {
 
 // function min
 
-//function min() {
-//	var i = 0, result;
-//	var arr = [];
-//
-//	console.log(arguments);
-////	if arguments.length = 0 console.log('empty') return;
-//	
-//	for (i = 0; i < arguments.length; i++) {
-//		arr[i] = parseFloat(arguments[i]);
-//	}	
-//	
-//	result = arr[0];
-//	
-//	arr.reduce(function(previousValue, currentItem) {
-//		if (currentItem <= previousValue)	previousValue = currentItem;
-//		result = previousValue;
-//		return result;
-//	});
-//	return result;
-//}
-
 function min(arr) {
-	var i = 0, min, result;
+	if (!arguments.length) {
+		return;
+	}
+	var i = arguments.length, result;
+	while (i > 0) {
+		if (typeof(arr[i]) === "number") {
+			result = arr[i];
+		}
+		i--;
+	}
+	
 	for (i = 0; i < arr.length; i++) {
-		min = arr[0];
+		// if (!typeof(arr[i])) {
+		if (typeof(arr[i] === "number")) {
+			if (arr[i] < result)	{
+				result = arr[i];
+			}
+		}
 	}
-	for (i = 1; i < arr.length; i++) {
-		if (arr[i] < min)	min = arr[i];
-	}
-	result = min;
 	return result;
 }
 
 // funstion max
 
-//function max() {
-//	var i = 0, max, result;
-//	for (i = 0; i < arguments.length; i++) {
-//		max = arguments[0];
-//	}
-//	for (i = 1; i < arguments.length; i++) {
-//		if (arguments[i] > max)	max = arguments[i];
-//	}
-//	result = max;
-//	return result;
-//}
-
 function max(arr) {
+	if (!arguments.length) {
+		return;
+	}
 	var i = 0, max, result;
 	for (i = 0; i < arr.length; i++) {
-		max = arr[0];
+		if (!typeof(arr[i])) {
+			continue;
+		}
+		max = arr[i];
 	}
-	for (i = 1; i < arr.length; i++) {
-		if (arr[i] > max)	max = arr[i];
+	for (i = 0; i < arr.length; i++) {
+		if (!typeof(arr[i])) {
+			continue;
+		}
+		if (arr[i] > max)	{
+			max = arr[i];
+		}
 	}
 	result = max;
 	return result;
